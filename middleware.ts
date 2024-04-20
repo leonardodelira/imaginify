c79698f34845106440bc15bde73b41cd08ac55e3
leonardodelira <leonardo.dlira@mercadolivre.com>
2024-04-20 15:40:24 -0300

    feat: clerk authentication

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/((?!sign-in|sign-up).*)",
+  "/api",
+  "/trpc",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!.*\\..*|_next).*)", // Don't run middleware on static files
+    "/", // Run middleware on index page
+    "/(api|trpc)(.*)",
+  ], // Run middleware on API routes
+};

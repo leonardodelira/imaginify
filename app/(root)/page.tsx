6f0c80de38dfdafb0dbcc1c0e907ea3f523b71a5
leonardodelira <leonardo.dlira@mercadolivre.com>
2024-04-20 17:52:14 -0300

    feat: database and models setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <p>Home</p>
-      <UserButton />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
